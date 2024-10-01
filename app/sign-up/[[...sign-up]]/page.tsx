4fcda7d07a5f9f95b85ff2f46400b3e14eac7849
Ankita Kulkarni <kulkarni.ankita09@gmail.com>
2024-10-01 00:51:31 -0400

    Auth Styling

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignUp } from '@clerk/nextjs';
 
 export default function Page() {
-  return <SignUp />;
+  return (
+    <div className="flex min-h-screen p-12 justify-center">
+      <SignUp />
+    </div>
+  );
 }
